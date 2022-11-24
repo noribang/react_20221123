493419e8ef8c38d70c97746ee09b33d56a155443
hoonkim <hoonk9387@gmail.com>
2022-11-23 22:18:19 -0600

    added 2 functions to app.js

--- src/App.js
+++ src/App.js
@@ -11,10 +11,26 @@ const user = {
 
 const className = "about-me-item"
 
+
+
+function createCard() {
+  return 1+1;
+}
+
+function getColor(isRed) {
+  if (isRed) {
+    return "red"
+  } else {
+    return "blue"
+  }
+}
+
 function App() {
   return (
     <React.Fragment>
       <h1>About Me</h1>
+      <p>{getColor()}</p>
+      <p>{createCard()}</p>
       <ul id="about-me">
         <li className={className}>name: {user.name}</li>
         <li className={className}>position: {user.position}</li>
