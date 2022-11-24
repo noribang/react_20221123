4294616a3083d7d79a27714f0ee80f1396900db0
hoonkim <hoonk9387@gmail.com>
2022-11-23 20:57:11 -0600

    Add ul and li elements in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>About Me</h1>
+      <ul id="about-me">
+        <li>Ian</li>
+        <li>Lead Instructor</li>
+        <li>Suttons Bay, MI</li>
+      </ul>
     </div>
   );
 }
