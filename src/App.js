1982bc33189692e705b07529aafdb4bd025214d6
hoonkim <hoonk9387@gmail.com>
2022-11-23 21:03:12 -0600

    added object literal to use for li elements

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 
+const user = {
+  name: "John",
+  position: "Head Cook",
+  hometown: "Los Angeles, CA"
+}
+
 function App() {
   return (
     <div className="App">
       <h1>About Me</h1>
       <ul id="about-me">
-        <li>Ian</li>
-        <li>Lead Instructor</li>
-        <li>Suttons Bay, MI</li>
+        <li>{user.name}</li>
+        <li>{user.position}</li>
+        <li>{user.hometown}</li>
       </ul>
     </div>
   );
